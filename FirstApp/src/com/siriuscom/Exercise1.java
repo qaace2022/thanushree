205f16e6ee67f29678bc7f5e41ac1d8a9255235c
thanushree17 <thanushree1795@gmail.com>
2022-02-14 15:20:02 +0530

    Create Exercise1.java
    Exercise 1

---
+++ FirstApp/src/com/siriuscom/Exercise1.java
@@ -0,0 +1,13 @@
+package com.siriuscom;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=15;
+		int c=a*b;
+		System.out.println("The Product is "+c);
+	}
+
+}
