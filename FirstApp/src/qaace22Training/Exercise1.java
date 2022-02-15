2d2d6be6fd2363b1f699cafd7a8e57ed957b1557
thanushree17 <thanushree1795@gmail.com>
2022-02-15 17:48:43 +0530

    Update Exercise1.java
    updated

--- FirstApp/src/qaace22Training/Exercise1.java
+++ FirstApp/src/qaace22Training/Exercise1.java
@@ -1,9 +1,9 @@
-package com.siriuscom;
+package qaace22Training;
 
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a=10;
 		int b=15;
 		int c=a*b;
